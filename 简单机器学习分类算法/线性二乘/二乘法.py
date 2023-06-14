0659f66a6286cfdd307ddff94c9190dc8aa49f2d
Akira <124018024+Akira-uestc@users.noreply.github.com>
2023-06-14 10:51:20 +0800

    Update 二乘法.py

--- 简单机器学习分类算法/线性二乘/二乘法.py
+++ 简单机器学习分类算法/线性二乘/二乘法.py
@@ -1,1 +1,23 @@
+# 导入numpy库
+import numpy as np
 
+# 定义数据点
+x = np.array([随便输个数组])
+y = np.array([随便输个数组])
+
+# 计算x和y的均值
+x_mean = np.mean(x)
+y_mean = np.mean(y)
+
+# 计算x和y的协方差
+cov_xy = np.sum((x - x_mean) * (y - y_mean))
+
+# 计算x的方差
+var_x = np.sum((x - x_mean) ** 2)
+
+# 根据最小二乘法公式，计算斜率和截距
+slope = cov_xy / var_x
+intercept = y_mean - slope * x_mean
+
+# 打印结果
+print(f"y = {slope:.2f} * x + {intercept:.2f}")
